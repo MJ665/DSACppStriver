01965e8dc8d051d05673995f22e18c448b78048f
Meet Jain <meetj665@gmail.com>
2024-02-29 03:41:46 +0530

    001cp

---
+++ 001.cpp
@@ -0,0 +1,12 @@
+/**
+    * * Highlight: STL standard template library
+**/
+
+# include"bits/stdc++.h"
+using namespace std;
+int main () {
+  int i ;
+  cin>>i;
+  cout<<"hey this is "<<i<<endl; 
+    return 0;
+}
